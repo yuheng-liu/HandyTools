ecedbedc951e9c5877d532af38146e696c42d95c
yuheng_keyun_house <42352679+yuheng-liu@users.noreply.github.com>
2020-10-11 00:00:22 +0800

    Add feature
    Added new home fragment, that can navigate to bill fragment instead

--- app/src/main/java/com/liuyuheng/handytools/ui/HomeFragment.kt
+++ app/src/main/java/com/liuyuheng/handytools/ui/HomeFragment.kt
@@ -5,11 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.liuyuheng.handytools.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        button_bill_calculator.setOnClickListener { findNavController(this).navigate(R.id.action_homeFragment_to_billTypeFragment) }
     }
 }
