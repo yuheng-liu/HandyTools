abd847fb461173d02edfef07edff33671cf84a35
yuheng_home <liu_yuheng@ctl.creative.com>
2020-10-22 00:45:29 +0800

    Update
    1. Updated android version to 11 (SDK 30), and other dependency versions
    2. Added number of people spinner

--- app/src/main/java/com/liuyuheng/handytools/ui/bill/BillTypeFragment.kt
+++ app/src/main/java/com/liuyuheng/handytools/ui/bill/BillTypeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.liuyuheng.common.extensions.visible
 import com.liuyuheng.handytools.R
@@ -18,7 +19,12 @@ class BillTypeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupUi()
         setupListeners()
+    }
+
+    private fun setupUi() {
+        spinner_number_of_people.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, (1..20).toList())
     }
 
     private fun setupListeners() {
